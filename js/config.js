5a6d190681b4fa0ef4bca525e68647821d787e13
yusuf ali sancak <yusufali.sancak0334@gmail.com>
2023-03-19 18:00:45 +0300

    initial commit

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var config = {
+    "loop" : 1, // reklam var ise 1, yoksa 0
+    "picOrVid" : "video", // fotoğraf : picture, video : video
+    "src" : "billboard/1.mov", // dosya konumu
+    "exchangeTime" : 3000, // kur tablosu gösterme süresi
+    "billboardTime" : 3000 // reklam gösterme süresi
+}
+
+export default config 
---
+++ js/exchanges.js
@@ -0,0 +1,15 @@
+var exchanges = {
+"TRY" : "0",
+"USD" : "0",
+"GBP" : "0",
+"DKK" : "0",
+"JPY" : "0",
+"NOK" : "0",
+"SEK" : "0",
+"CNY" : "0",
+"CAD" : "0",
+"SAR" : "0",
+"CHF" : "0"
+}
+
+export default exchanges
